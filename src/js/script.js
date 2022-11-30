d941f4fed511a173270ee577825a55ee238a2680
Ivan Kubrak <ivankubrak@Ivans-MacBook-Pro.local>
2022-11-30 20:17:23 +0200

    feedback animation added

--- src/js/script.js
+++ src/js/script.js
@@ -107,5 +107,22 @@ document.querySelector('.next').addEventListener('click', function () {
 		return false;
 	});
 
+	// Smouth scroll and pageup
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href^='#']").click(function() {
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+
+	new WOW().init();
 })(jQuery);
 
